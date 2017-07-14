010cde40f528372c77bae3cebc9cb2c579292063
Trevor Hamilton <tjh1003@sru.edu>
2017-07-14 12:36:21 -0400

    Added API Keys
    I have setup a dummy file to make it easier for others to build. Note
    you will need to generate your own keys from each service.

---
+++ APIKeys.cs
@@ -0,0 +1,28 @@
+﻿namespace WebNowPlaying
+{
+    class APIKeys
+    {
+        public class Spotify
+        {
+            //Client ID and secret key for spotify API
+            //Note: You will need to generate your own when building
+            public static string spotifyClientID = "";
+            public static string spotifyClientSecret = "";
+        }
+
+        public class Youtube
+        {
+            //For eventual use with the youtube API
+            //Note: You will need to generate your own when building
+            public static string youtubeAPIKey = "";
+        }
+
+        public class Twitch
+        {
+            //For eventual use with the twitch API
+            //Note: You will need to generate your own when building
+            public static string twitchClientID = "";
+            public static string twitchClientSecret = "";
+        }
+    }
+}
